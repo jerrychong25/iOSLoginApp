24e23e67416a9cfb486516a55bb5d7a263927cb4
cnvery <15714095+jerrychong25@users.noreply.github.com>
2018-08-16 21:06:07 +0800

    Added + and - buttons

--- app/src/main/java/com/example/android/justjavaapp/MainActivity.java
+++ app/src/main/java/com/example/android/justjavaapp/MainActivity.java
@@ -22,6 +22,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int numberOfCoffees = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +34,20 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2);
-        displayPrice(2*5);
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * 5);
+    }
+
+    public void increment(View view) {
+        numberOfCoffees += 1;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * 5);
+    }
+
+    public void decrement(View view) {
+        numberOfCoffees -= 1;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * 5);
     }
 
     /**
