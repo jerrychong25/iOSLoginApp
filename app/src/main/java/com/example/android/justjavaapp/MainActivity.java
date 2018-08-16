97ee2d4e992d3c1169ba4f144deaaaa699090f49
honfong <15714095+jerrychong25@users.noreply.github.com>
2018-08-16 13:52:24 +0800

    Add Price text bottom of Quantity

--- app/src/main/java/com/example/android/justjavaapp/MainActivity.java
+++ app/src/main/java/com/example/android/justjavaapp/MainActivity.java
@@ -15,6 +15,8 @@ import android.widget.TextView;
 
 import com.example.android.justjavaapp.R;
 
+import java.text.NumberFormat;
+
 /**
  * This app displays an order form to order coffee.
  */
@@ -30,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        display(2);
+        displayPrice(2*5);
     }
 
     /**
@@ -39,5 +42,13 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
