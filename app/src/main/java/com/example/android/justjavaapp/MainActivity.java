3907897df33bd420924cb5a6a7ce85845bacf6e5
cnvery <15714095+jerrychong25@users.noreply.github.com>
2018-08-22 00:49:04 +0800

    Modified displayQuantity() function name

--- app/src/main/java/com/example/android/justjavaapp/MainActivity.java
+++ app/src/main/java/com/example/android/justjavaapp/MainActivity.java
@@ -42,18 +42,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void increment(View view) {
         quantity += 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view) {
         quantity -= 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
