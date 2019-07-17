e0fb207e8ee2809da0eb5dc2e019e9a40fa3898e
jerrychong25 <15714095+jerrychong25@users.noreply.github.com>
2019-07-17 09:59:48 +0800

    Fixed password with secureTextEntry
    -Added password with secureTextEntry in LoginScreen.storyboard
    -Fixed full_name and confirm_password not converting to text issue in RegisterViewController.swift
    -Modified Register Button selector from didTapLoginButton() to didTapRegisterButton() in LoginScreen.storyboard
    -Modified pass variable name to password in LoginViewController.swift
    -Added debug log in LoginViewController.swift and RegisterViewController.swift

--- LoginScreen/ViewControllers/LoginViewController.swift
+++ LoginScreen/ViewControllers/LoginViewController.swift
@@ -25,11 +25,15 @@ class LoginViewController: UIViewController {
         NSLog("LoginViewController didTapLoginButton()")
         
         // Regular login attempt. Add the code to handle the login by email and password.
-        guard let email = emailTextField.text, let pass = passwordTextField.text else {
+        guard let email = emailTextField.text, let password = passwordTextField.text else {
             // It should never get here
             return
         }
-        didLogin(method: "email and password", info: "\nEmail: \(email) \n Password: \(pass)")
+        
+        NSLog("LoginViewController email: " + email)
+        NSLog("LoginViewController password: " + password)
+        
+        didLogin(method: "email and password", info: "\nEmail: \(email) \n Password: \(password)")
     }
     
     @IBAction func didTapRegisterButton(_ sender: RegisterButton) {
@@ -56,14 +60,14 @@ class LoginViewController: UIViewController {
     private func didLogin(method: String, info: String) {
         NSLog("LoginViewController didLogin()")
         
-////        let message = "Successfully logged in with \(method). \n" + info
-//        let message = info
-//        let alert = UIAlertController(title: "Success", message: message, preferredStyle: UIAlertController.Style.alert)
-//        alert.addAction(UIAlertAction(title: "Done", style: UIAlertAction.Style.default, handler: nil))
-//        self.present(alert, animated: true, completion: nil)
+//        let message = "Successfully logged in with \(method). \n" + info
+        let message = info
+        let alert = UIAlertController(title: "Success", message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Done", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
         
-        // Switch To Web Page
-        performSegue(withIdentifier: "webView", sender: self)
+//        // Switch To Web Page
+//        performSegue(withIdentifier: "webView", sender: self)
     }
     
     private func didRegister() {
